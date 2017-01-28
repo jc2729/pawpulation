75ceb14ac44f0c11e777e3876ae43d17147d2f50
Sourabh Chakraborty <sc2356@cornell.edu>
2017-01-28 14:24:03 -0500

    Basic login (client side)

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,11 +1,81 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
-    private Stage loginStage;
-    
-    public void setStage(Stage stage) {
-        loginStage = stage;
-    }
+	private Stage loginStage;
+	private final String baseURL = ""; //TODO
+	private int sessionID;
+
+	@FXML
+	private TextField usernameField;
+
+	@FXML
+	private TextField passwordField;
+
+	//Username field
+	//Password field
+
+	public void setStage(Stage stage) {
+		loginStage = stage;
+	}
+
+	//button click method
+	//get username from username field
+	//get password from password field
+	//convert to username/password combination class and send to server
+
+	@FXML
+	private void handleLoginButton(final ActionEvent event){
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+
+		if(username.equals("") || password.equals("")){
+			Alert a = new Alert(AlertType.ERROR, "Username and password cannot be empty.");
+			a.showAndWait();
+			return;
+		}
+
+		JsonObject loginCreds = new JsonObject();
+		loginCreds.addProperty("username", username);
+		loginCreds.addProperty("password", password);
+
+		try{
+			URL url = new URL(baseURL + "/login");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestMethod("POST");
+			PrintWriter w = new PrintWriter(connection.getOutputStream());
+			w.println(loginCreds);
+			w.flush();
+
+			if(connection.getResponseCode() == 201){
+				BufferedReader r =
+						new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				sessionID = r.read();
+			}
+			else{
+				Alert a = new Alert(AlertType.ERROR, "Could not connect to server.");
+				a.showAndWait();
+			}
+		}
+		catch(IOException e){
+
+		}
+	}
 }

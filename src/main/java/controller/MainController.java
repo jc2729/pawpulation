dffe514cf8568b9c57b720014995cec98fdbbbe1
Avani <ab2387@cornell.edu>
2017-01-28 14:14:55 -0500

    Added fxids to welcome.fxml

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,5 +1,73 @@
 package controller;
 
-public class MainController {
+import java.io.File;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+
+public class MainController {
+    private View view;
+    
+    //fields for fxml elements
+    @FXML
+    private Button importButton;
+    
+    @FXML
+    private Button searchButton;
+    
+    @FXML
+    private Button settingsButton;
+    
+    @FXML
+    private SplitPane splitPane;
+
+    /**
+     * Presents a file chooser and loads file 
+     */
+    
+    @FXML
+    private void handleImportAction(ActionEvent event) {
+        //load file chooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Import File");
+        File file = fileChooser.showOpenDialog(splitPane.getScene().getWindow());
+        
+        //parse file into JSON object
+        
+        //submit POST request
+    }
+    
+    @FXML
+    private void handleSearchAction(ActionEvent event) {
+        //make search pane visible
+        
+        //pass control to SearchController
+    }
+    
+    @FXML
+    private void handleSettingsAction(ActionEvent event) {
+        
+    }
+    
+    
+    /**
+     * Parse file into JSON object and submit POST request to add entries to database
+     */
+    public void addToDatabase(File file) {
+        
+    }
+    
+    /**
+     * Submit GET request to server to retrieve information from database and update view
+     */
+    public void search() {
+        
+    }
+    
+    public void updateView() {
+        
+    }
+    
 }

e5f73e1de958d93f55eeb0a377c2a5a51e5bbdcc
Sourabh Chakraborty <sc2356@cornell.edu>
2017-01-28 16:10:03 -0500

    login and main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,15 @@
 package main;
 
+import javafx.application.*;
+import server.Server;
+import controller.StartView;
+
 public class Main {
+	
+	public static void main(String [] args){
+		Server server = new Server();
+		server.run();
+		StartView.main(args);
+	}
 
 }

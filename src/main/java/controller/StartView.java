8d2e230b240d35a6f9961d5624ceb5f0dc66a199
Avani <ab2387@cornell.edu>
2017-01-28 13:17:00 -0500

    Wrote StartView, added to LoginView

--- src/main/java/controller/StartView.java
+++ src/main/java/controller/StartView.java
@@ -1,5 +1,24 @@
 package controller;
 
-public class StartView {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class StartView extends Application {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
+        GridPane root = loader.load();
+        ((LoginController) loader.getController()).setStage(stage);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
